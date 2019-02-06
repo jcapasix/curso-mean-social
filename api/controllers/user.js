b41f24224af35749889544d37f266c622f56b26f
jcapasix <jcapasix@gmail.com>
2019-02-06 12:32:50 -0500

    Servicio y tokens JWT

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,6 +2,9 @@
 var bcrypt = require('bcrypt-nodejs')
 var User = require('../models/user');
 
+var jwt = require('../services/jwt');
+
+
 function home(req, res){
 	res.status(200).send({
 		message: 'Hola mundo desde el servidor de NodeJS'
@@ -74,9 +77,18 @@ function loginUser(req, res){
 		if(user){
 			bcrypt.compare(password, user.password, (err, check) => {
 				if(check){
-					//devolvemos el usuario
-					user.password = undefined
-					return res.status(200).send({user})
+
+					if(params.gettoken){
+						//generar y devolver token
+						return res.status(200).send({
+							token: jwt.createToken(user)
+						});
+					}
+					else{
+						//devolvemos datos del usuario
+						user.password = undefined
+						return res.status(200).send({user})
+					}
 				}
 				else{
 					return res.status(404).send({message: 'El usuario no se ha podido identificar'})
