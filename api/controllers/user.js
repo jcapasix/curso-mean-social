28a8898cf72b49e0d4167925d77d27fd6c6a7b45
jcapasix <jcapasix@gmail.com>
2019-02-09 02:33:16 -0500

    Middleware de autenticación. Ruta para devolver los datos del usuario. Usuarios paginados. Actualizar los datos del usuario. Subir avatar de usuario. Primera parte. La imagen de usuario. Segunda parte. Devolver imágenes de usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,12 @@
 'use strict'
 var bcrypt = require('bcrypt-nodejs')
+var mongoosePaginate = require('mongoose-pagination');
 var User = require('../models/user');
-
 var jwt = require('../services/jwt');
+var fs = require('fs');
+var path = require('path');
 
-
+//metodos de prueba
 function home(req, res){
 	res.status(200).send({
 		message: 'Hola mundo desde el servidor de NodeJS'
@@ -17,6 +19,7 @@ function pruebas(req, res){
 	});
 }
 
+//registro
 function saveUser(req, res){
 	var params = req.body;
 	var user = new User();
@@ -66,6 +69,7 @@ function saveUser(req, res){
 
 }
 
+//login
 function loginUser(req, res){
 	var params = req.body;
 	var email = params.email;
@@ -102,9 +106,134 @@ function loginUser(req, res){
 }
 
 
+//conseguir datos de un usuario
+
+function getUser(req, res){
+
+	var userId = req.params.id;
+	User.findById(userId, (err, user)=> {
+		if(err) return res.status(500).send({message: 'Error en ela peticion'});
+		if(!user) return res.status(404).send({message: 'El usuario no existe'});
+
+		return res.status(200).send({user});
+	});
+}
+
+function getUsers(req, res){
+
+	var identity_user_id = req.user.sub;
+	var page = 1;
+
+	if(req.params.page){
+		page = req.params.page;
+	}
+
+	var itemsPerPage = 5;
+
+	User.find().sort('_id').paginate(page, itemsPerPage, (err, users, total) =>{
+		if(err) return res.status(500).send({message: 'Error en ela peticion'});
+		if(!users) return res.status(404).send({message: 'No hay usuarios disponibles'});
+
+		return res.status(200).send({
+			users, 
+			total,
+			pages: Math.ceil(total/itemsPerPage)
+		});
+
+	});
+}
+
+function updateUser(req, res){
+
+	var userId = req.params.id;
+	var update = req.body;
+
+	console.log(update)
+
+	//borrar propiedad password
+	delete update.password;
+
+	if(userId != req.user.sub){
+		return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+	}
+
+	User.findByIdAndUpdate(userId, update, {new: true}, (err, userUpdated) => {
+		if(err) return res.status(500).send({message: 'Error en ela peticion'});
+		if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+		return res.status(200).send({ user: userUpdated});
+	});
+}
+
+function uploadImage(req, res){
+	var userId = req.params.id;
+
+	if(userId != req.user.sub){
+		return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+	}
+	
+	if(req.files){
+		var file_path = req.files.image.path;
+		console.log(file_path);
+		var file_split = file_path.split('\/');
+		console.log(file_split);
+		var file_name = file_split[2];
+		console.log(file_name);
+
+
+		var ext_split = file_path.split('\.');
+		var file_ext = ext_split[1];
+
+		if(userId != req.user.sub){
+			return removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar los datos del usuario');
+		}
+
+		if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif' || file_ext == 'jpeg'){
+			//Actualizar documento de usuario logueado
+			User.findByIdAndUpdate(userId, {image: file_name}, {new: true}, (err, userUpdated) => {
+				if(err) return res.status(500).send({message: 'Error en ela peticion'});
+				if(!userUpdated) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+				return res.status(200).send({ user: userUpdated});
+			});
+		}
+		else{
+			return removeFilesOfUploads(res, file_path, 'Extension no valida');
+		}
+
+	}
+	else{
+		return res.status(200).send({ message: 'No se han subido imagenes'});
+	}
+}
+
+function removeFilesOfUploads(res, file_path, message){
+	fs.unlink(file_path, (err)=>{
+		return res.status(200).send({ message: message});
+	})
+}
+
+function getImageFile(req, res){
+	var image_file = req.params.imageFile;
+	var path_file = './uploads/users/'+image_file;
+
+	fs.exists(path_file, (exists) => {
+		if(exists){
+			res.sendFile(path.resolve(path_file));
+		}
+		else{
+			res.status(200).send({message: 'No existe la imagen ...'});
+		}
+	});
+}
+
 module.exports = {
 	home,
 	pruebas,
 	saveUser,
-	loginUser
+	loginUser,
+	getUser,
+	getUsers,
+	updateUser,
+	uploadImage,
+	getImageFile
 }
