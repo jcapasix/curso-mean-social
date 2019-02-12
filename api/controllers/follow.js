4714d0c943eeadc61c029c0a09755995c3f694d4
jcapasix <jcapasix@gmail.com>
2019-02-11 23:45:28 -0500

    Seguir a un usuario

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -10,6 +10,21 @@ function prueba(req, res){
     res.status(200).send({message: 'Hola Mundo desde el controlador follows'})
 }
 
+function saveFollow(req, res){
+    var params = req.body;
+
+    var follow = new Follow();
+    follow.user = req.user.sub;
+    follow.followed = params.followed;
+
+    follow.save((err, followStored)=>{
+        if(err) return res.status(500).send({message: 'Error al guardar el seguimeinto'});
+        if(!followStored) return res.status(404).send({message: 'El seguimiento no se ha guardado'});
+        return res.status(200).send({follow: followStored});
+    });
+}
+
 module.exports = {
-    prueba
+    prueba,
+    saveFollow
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -7,6 +7,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
-
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 
 module.exports = api;
