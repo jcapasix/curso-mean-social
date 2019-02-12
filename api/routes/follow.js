ad2882d51e081f71a352295ddc47060540b7db9f
jcapasix <jcapasix@gmail.com>
2019-02-11 23:52:20 -0500

    Dejar de seguir usuarios

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -8,5 +8,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
+api.delete('/delete-follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 
 module.exports = api;
