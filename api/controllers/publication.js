08533c749a6b3884f64a6289b97dbc00abaf2743
jcapasix <jcapasix@gmail.com>
2019-02-12 17:10:03 -0500

    Guardar nuevas publicaciones

--- api/controllers/publication.js
+++ api/controllers/publication.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var fs = require('fs');
+var moment = require('moment');
 var mongoosePaginate = require('mongoose-pagination');
 
 var User = require('../models/user');
@@ -14,6 +15,26 @@ function probando(req, res){
     });
 }
 
+function savePublication(req, res){
+    var params = req.body;
+
+    if(!params.text) return res.status(200).send({message: 'Debes enviar un texto'});
+
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+
+    publication.save((err, publicationStored)=>{
+        if(err) return res.status(500).send({message: 'Error al guardar la publicacion'});
+        if(!publicationStored) return res.status(404).send({message: 'La publicacion No ha sido guardada'});
+
+        return res.status(200).send({publication: publicationStored});
+    });
+}
+
 module.exports = {
-    probando
+    probando,
+    savePublication
 }
