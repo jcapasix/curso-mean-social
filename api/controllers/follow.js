045e4b971027751c211c36ec68c79bd48fabdd2d
jcapasix <jcapasix@gmail.com>
2019-02-12 00:24:53 -0500

    Listado de usuarios que sigo

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -34,8 +34,37 @@ function deleteFollow(req, res){
     });
 }
 
+function getFollowingUsers(req, res){
+
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({user:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total)=>{
+        if(err) return res.status(500).send({message: 'Error en el servidor'});
+        if(!follows) return res.status(404).send({message: 'No estas siguiendo a nungun usuario'});
+
+        return res.status(200).send({
+            total:total,
+            pages:Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
 module.exports = {
     prueba,
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -9,5 +9,6 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/pruebas-follow', md_auth.ensureAuth, FollowController.prueba);
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/delete-follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
 
 module.exports = api;
