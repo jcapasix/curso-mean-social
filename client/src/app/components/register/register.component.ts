715e2ef6eb613c506d3649ba3a3cfe7c8912edc7
jcapasix <jcapasix@gmail.com>
2019-02-13 15:39:47 -0500

    Recibir datos del formulario

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -22,4 +22,8 @@ export class RegisterComponent implements OnInit{
     ngOnInit(){
         console.log('Componente de registro cargado...')
     }
+
+    onSubmit(){
+        console.log(this.user);
+    }
 }
