df9c9e68af1ce4bd51013ed261f07dada04bd64e
jcapasix <jcapasix@gmail.com>
2019-02-15 23:50:37 -0500

    boton de seguir

--- client/src/app/components/users/users.component.ts
+++ client/src/app/components/users/users.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit} from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { User } from '../../models/user';
+import { Follow } from '../../models/follow';
 import { UserService } from '../../services/user.service';
+import { FollowService } from '../../services/follow.service';
 import { GLOBAL } from '../../services/global';
 
 
 @Component({
     selector: "users",
     templateUrl: './users.component.html',
-    providers: [UserService]
+    providers: [UserService, FollowService]
 })
 
 export class UsersComponent implements OnInit{
@@ -31,7 +33,8 @@ export class UsersComponent implements OnInit{
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
-        private _userService:UserService
+        private _userService: UserService,
+        private _followService: FollowService
     ){
         this.title = 'Gente';
         this.user = this._userService.getIdentity();
@@ -105,5 +108,30 @@ export class UsersComponent implements OnInit{
 
     mouseLeave(user_id){
         this.followUserOver = 0;
+    }
+
+    followUser(followed){
+
+        var follow = new Follow('', this.identity._id, followed);
+
+        this._followService.addFollow(this.token, follow).subscribe(
+            response => {
+                if(!response.follow){
+                    this.status = 'error';
+                }
+                else{
+                    this.status = 'success';
+                    this.follows.push(followed);
+                }
+            },
+            error => {
+                var errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if(errorMessage != null){
+                    this.status = 'error';
+                }
+            }
+        );
     }
 }
